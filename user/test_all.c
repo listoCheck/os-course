bf3dcd63e683e6c99652a02932366c72fe3fc468
ArtemAmuz <92678018+listoCheck@users.noreply.github.com>
2025-12-05 11:30:19 +0300

    Refactor main function to handle command line args

--- user/test_all.c
+++ user/test_all.c
@@ -4,48 +4,46 @@
 #include "shm.h"
 #include "sem.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if(argc < 2){
+        printf("Usage: %s <role>\n", argv[0]);
+        exit(1);
+    }
+
     int shmid = shmget(1234, 8192, IPC_CREAT);
     if(shmid < 0){
-        write(1, "shmget failed\n", 14);
+        printf("shmget failed\n");
         exit(1);
     }
 
     char *p = shmat(shmid, 0);
     if(!p){
-        write(1, "shmat failed\n", 13);
+        printf("shmat failed\n");
         exit(1);
     }
 
     sem_t s = ksem_open("/s", 1, 1);
-    if(s < 0) { 
-        write(1, "ksem_open failed\n", 17);
-        exit(1); 
+    if(s < 0){
+        printf("ksem_open failed\n");
+        exit(1);
     }
 
-    if(fork() == 0){
+    if(strcmp(argv[1], "A") == 0){
         ksem_wait(s);
-        p[0] = 'C';
-        write(1, "child wrote C\n", 14);
+        p[0] = 'A';
+        printf("Process A wrote A\n");
         ksem_post(s);
-        ksem_close(s);
-        exit(0);
-    } else {
+    } else if(strcmp(argv[1], "B") == 0){
         ksem_wait(s);
-        p[0] = 'P';
-        write(1, "parent wrote P\n", 15);
+        p[0] = 'B';
+        printf("Process B wrote B\n");
         ksem_post(s);
-
-        wait(0);
-
-        write(1, "final value in shared memory: ", 29);
-        write(1, &p[0], 1);
-        write(1, "\n", 1);
-
-        ksem_close(s);
-        ksem_unlink("/s");
-        shmdt(p);
     }
+    ksem_wait(s);
+    printf("Process %s sees value: %c\n", argv[1], p[0]);
+    ksem_post(s);
 
+    ksem_close(s);
+    shmdt(p);
     exit(0);
 }
