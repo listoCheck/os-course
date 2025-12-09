59f08ef67251bfe4b62dcd8b42e0222689dfe333
Artem <v.amuzinsky@yandex.ru>
2025-12-09 20:57:54 +0300

    http update
    Signed-off-by: Artem <v.amuzinsky@yandex.ru>

--- lab/vtfs/source/http.c
+++ lab/vtfs/source/http.c
@@ -1,214 +1,186 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/socket.h>
+#include <linux/in.h>
+#include <linux/net.h>
+#include <linux/kvec.h>
+#include <linux/inet.h>
+#include <linux/uaccess.h>
 #include "http.h"
 
 const char *SERVER_IP = "0.0.0.0";
 const int SERVER_PORT = 8080;
 
-// callee should call free_request on received buffer
-int fill_request(struct kvec *vec, const char *token, const char *method,
+// callee should call kfree on vec->iov_base
+static int fill_request(struct kvec *vec, const char *token, const char *method,
                  size_t arg_size, va_list args) {
-  // 2048 bytes for URL and 64 bytes for anything else
-  char *request_buffer = kzalloc(2048 + 64, GFP_KERNEL);
-  if (request_buffer == 0) {
-    return -ENOMEM;
-  }
+    char *request_buffer = kzalloc(2048 + 64, GFP_KERNEL);
+    if (!request_buffer) {
+        return -ENOMEM;
+    }
 
-  strcpy(request_buffer, "GET /api/");
-  strcat(request_buffer, method);
+    strcpy(request_buffer, "GET /api/");
+    strcat(request_buffer, method);
+    strcat(request_buffer, "?token=");
+    strcat(request_buffer, token);
 
-  strcat(request_buffer, "?token=");
-  strcat(request_buffer, token);
+    for (size_t i = 0; i < arg_size; i++) {
+        strcat(request_buffer, "&");
+        strcat(request_buffer, va_arg(args, char *));
+        strcat(request_buffer, "=");
+        strcat(request_buffer, va_arg(args, char *));
+    }
 
-  for (int i = 0; i < arg_size; i++) {
-    strcat(request_buffer, "&");
-    strcat(request_buffer, va_arg(args, char *));
-    strcat(request_buffer, "=");
-    strcat(request_buffer, va_arg(args, char *));
-  }
+    strcat(request_buffer, " HTTP/1.1\r\nHost:");
+    strcat(request_buffer, SERVER_IP);
+    strcat(request_buffer, "\r\nConnection: close\r\n\r\n");
 
-  strcat(request_buffer, " HTTP/1.1\r\nHost:");
-  strcat(request_buffer, SERVER_IP);
-  strcat(request_buffer, "\r\nConnection: close\r\n\r\n");
+    vec->iov_base = request_buffer;
+    vec->iov_len = strlen(request_buffer);
 
-  memset(vec, 0, sizeof(struct kvec));
-  vec->iov_base = request_buffer;
-  vec->iov_len = strlen(request_buffer);
-
-  return 0;
+    return 0;
 }
 
-int receive_all(struct socket *sock, char *buffer, size_t buffer_size) {
-  struct msghdr hdr;
-  struct kvec vec;
+static int receive_all(struct socket *sock, char *buffer, size_t buffer_size) {
+    struct msghdr msg;
+    struct kvec vec;
+    int total_read = 0;
 
-  int read = 0;
+    while (total_read < buffer_size) {
+        memset(&msg, 0, sizeof(msg));
+        vec.iov_base = buffer + total_read;
+        vec.iov_len = buffer_size - total_read;
 
-  while (read < buffer_size) {
-    memset(&hdr, 0, sizeof(struct msghdr));
-    memset(&vec, 0, sizeof(struct kvec));
-    vec.iov_base = buffer + read;
-    vec.iov_len = buffer_size - read;
-    int ret = kernel_recvmsg(sock, &hdr, &vec, 1, vec.iov_len, 0);
-    if (ret == 0) {
-      break;
-    } else if (ret < 0) {
-      return -4;
+        int ret = kernel_recvmsg(sock, &msg, &vec, 1, vec.iov_len, 0);
+        if (ret == 0)
+            break; // EOF
+        else if (ret < 0)
+            return ret;
+        total_read += ret;
     }
-    read += ret;
-  }
 
-  return read;
+    return total_read;
 }
 
-int64_t parse_http_response(char *raw_response, size_t raw_response_size,
-                            char *response, size_t response_size) {
-  char *buffer = raw_response;
+static int64_t parse_http_response(char *raw, size_t raw_size, char *resp, size_t resp_size) {
+    char *p = raw;
+    char *line, *status;
+    int content_length = -1;
 
-  // Read Response Line
-  {
-    char *status_line = strsep(&buffer, "\r");
-    strsep(&status_line, " ");
-    if (status_line == 0) {
-      return -6;
-    }
-    char *status_code = strsep(&status_line, " ");
-    printk(KERN_INFO "Received response with status code %s\n", status_code);
-    if (strcmp(status_code, "200") != 0) {
-      return -5;
-    }
-  }
-
-  int length = -1;
-
-  while (true) {
-    if (buffer == 0) {
-      return -6;
-    }
-    char *header = strsep(&buffer, "\r");
-    ++header; // skip \n
-    if (strcmp(header, "") == 0) {
-      // end of headers
-      break;
-    }
-
-    if (strncmp(header, "Content-Length: ", 16) == 0) {
-      int error = kstrtoint(header + 16, 0, &length);
-      if (error != 0) {
+    line = strsep(&p, "\r\n");
+    if (!line)
         return -6;
-      }
-      printk(KERN_INFO "Received response with content length %d\n", length);
+
+    status = strsep(&line, " ");
+    if (!status)
+        return -6;
+    status = strsep(&line, " ");
+    if (!status)
+        return -6;
+
+    if (strcmp(status, "200") != 0)
+        return -5;
+
+    while (p && *p) {
+        line = strsep(&p, "\r\n");
+        if (!line || line[0] == '\0') break;
+
+        if (!strncmp(line, "Content-Length: ", 16)) {
+            if (kstrtoint(line + 16, 0, &content_length))
+                return -6;
+        }
     }
-  }
-  ++buffer; // skip last '\n'
 
-  if (length == -1) {
-    return -6;
-  }
+    if (content_length < (int)sizeof(int64_t))
+        return -7;
+    content_length -= sizeof(int64_t);
 
-  if (buffer + length > raw_response + raw_response_size) {
-    return -6;
-  }
+    if (content_length > (int)resp_size)
+        return -ENOSPC;
 
-  if (length < sizeof(int64_t)) {
-    return -7;
-  }
+    int64_t ret_val;
+    memcpy(&ret_val, p, sizeof(int64_t));
+    p += sizeof(int64_t);
+    memcpy(resp, p, content_length);
 
-  length -= sizeof(int64_t);
-
-  if (length > response_size) {
-    return -ENOSPC;
-  }
-
-  int64_t return_value;
-  memcpy(&return_value, buffer, sizeof(int64_t));
-
-  buffer += sizeof(int64_t);
-  memcpy(response, buffer, length);
-
-  return return_value;
+    return ret_val;
 }
 
 int64_t vtfs_http_call(const char *token, const char *method,
-                            char *response_buffer, size_t buffer_size,
-                            size_t arg_size, ...) {
-  struct socket *sock;
-  int64_t error;
+                       char *response_buffer, size_t buffer_size,
+                       size_t arg_size, ...) {
+    struct socket *sock;
+    struct sockaddr_in saddr;
+    int64_t ret;
+    int error;
 
-  error = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
-  if (error < 0) {
-    return -1;
-  }
+    error = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
+    if (error < 0) return -1;
 
-  struct sockaddr_in s_addr = {.sin_family = AF_INET,
-                               .sin_addr = {.s_addr = in_aton(SERVER_IP)},
-                               .sin_port = htons(SERVER_PORT)};
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(SERVER_PORT);
+    saddr.sin_addr.s_addr = in_aton(SERVER_IP);
 
-  error = kernel_connect(sock, (struct sockaddr *)&s_addr,
-                         sizeof(struct sockaddr_in), 0);
-  if (error != 0) {
+    if ((error = kernel_connect(sock, (struct sockaddr *)&saddr, sizeof(saddr), 0)) < 0) {
+        sock_release(sock);
+        return -2;
+    }
 
-    sock_release(sock);
-    return -2;
-  }
+    struct kvec vec;
+    va_list args;
+    va_start(args, arg_size);
+    error = fill_request(&vec, token, method, arg_size, args);
+    va_end(args);
 
-  struct kvec kvec;
-  va_list args;
-  va_start(args, arg_size);
-  error = fill_request(&kvec, token, method, arg_size, args);
-  va_end(args);
+    if (error) {
+        kernel_sock_shutdown(sock, SHUT_RDWR);
+        sock_release(sock);
+        return error;
+    }
 
-  if (error != 0) {
+    struct msghdr msg;
+    memset(&msg, 0, sizeof(msg));
+    error = kernel_sendmsg(sock, &msg, &vec, 1, vec.iov_len);
+    kfree(vec.iov_base);
+    if (error < 0) {
+        kernel_sock_shutdown(sock, SHUT_RDWR);
+        sock_release(sock);
+        return -3;
+    }
+
+    char *raw_buffer = kmalloc(buffer_size + 1024, GFP_KERNEL);
+    if (!raw_buffer) {
+        kernel_sock_shutdown(sock, SHUT_RDWR);
+        sock_release(sock);
+        return -ENOMEM;
+    }
+
+    int read_bytes = receive_all(sock, raw_buffer, buffer_size + 1024);
     kernel_sock_shutdown(sock, SHUT_RDWR);
     sock_release(sock);
-    return error;
-  }
+    if (read_bytes < 0) {
+        kfree(raw_buffer);
+        return -4;
+    }
 
-  struct msghdr msg;
-  memset(&msg, 0, sizeof(struct msghdr));
-
-  error = kernel_sendmsg(sock, &msg, &kvec, 1, kvec.iov_len);
-  kfree(kvec.iov_base);
-
-  if (error < 0) {
-    kernel_sock_shutdown(sock, SHUT_RDWR);
-    sock_release(sock);
-    return -3;
-  }
-
-  size_t raw_buffer_size = buffer_size + 1024; // add 1KB for HTTP headers
-  char *raw_response_buffer = kmalloc(raw_buffer_size, GFP_KERNEL);
-  if (raw_response_buffer == 0) {
-    kernel_sock_shutdown(sock, SHUT_RDWR);
-    sock_release(sock);
-    return -ENOMEM;
-  }
-  int read_bytes = receive_all(sock, raw_response_buffer, raw_buffer_size);
-
-  kernel_sock_shutdown(sock, SHUT_RDWR);
-  sock_release(sock);
-
-  if (read_bytes < 0) {
-    kfree(raw_response_buffer);
-    return -4;
-  }
-
-  error = parse_http_response(raw_response_buffer, read_bytes, response_buffer,
-                              buffer_size);
-
-  kfree(raw_response_buffer);
-  return error;
+    ret = parse_http_response(raw_buffer, read_bytes, response_buffer, buffer_size);
+    kfree(raw_buffer);
+    return ret;
 }
 
 void encode(const char *src, char *dst) {
-  while (*src != '\0') {
-    if ((*src >= '0' && *src <= '9') || (*src >= 'a' && *src <= 'z') ||
-        (*src >= 'A' && *src <= 'Z')) {
-      *dst = *src;
-      dst++;
-    } else {
-      sprintf(dst, "%%%02X", (unsigned char)*src);
-      dst += 3;
+    while (*src) {
+        if ((*src >= '0' && *src <= '9') ||
+            (*src >= 'a' && *src <= 'z') ||
+            (*src >= 'A' && *src <= 'Z')) {
+            *dst++ = *src;
+        } else {
+            sprintf(dst, "%%%02X", (unsigned char)*src);
+            dst += 3;
+        }
+        src++;
     }
-    src++;
-  }
-  *dst = '\0';
+    *dst = '\0';
 }
