0c0d28a1f8fe90701b662a88129095238d80a960
vboxuser <vboxuser@deb.myguest.virtualbox.org>
2025-12-09 22:39:59 +0300

    update

--- lab/vtfs/source/http.h
+++ lab/vtfs/source/http.h
@@ -1,12 +1,12 @@
 #ifndef VTFS_HTTP_H
 #define VTFS_HTTP_H
 
-#include <linux/inet.h>
+#include <linux/types.h>
 
 int64_t vtfs_http_call(const char *token, const char *method,
-                            char *response_buffer, size_t buffer_size,
-                            size_t arg_size, ...);
+                      char *response_buffer, size_t buffer_size,
+                      size_t arg_size, ...);
 
-void encode(const char *, char *);
+void encode(const char *src, char *dst);
 
 #endif // VTFS_HTTP_H
